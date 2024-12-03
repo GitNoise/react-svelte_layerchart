78ebc882015cfcea9050ab627643588f81d29146
Patrick Wojda <patrick@datastory.org>
2024-12-03 14:16:53 +0100

    Add 'broken' pages folder example

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,12 @@
 export default function Home() {
   return (
     <div>
-      <a href="/layercake">Test LayerCake</a>
+      <a href="/layerchart_app">Test LayerChart running in App folder</a>
+
       <br />
       <br />
-      <a href="/layerchart">Test LayerChart</a>
+
+      <a href="/layerchart_pages">Test LayerChart running in Pages folder</a>
     </div>
   );
 }
